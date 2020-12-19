afa7d85a3f06398a5c2ceeaa96535ffb08ad8b37
yamabudousukkashu <yamabudousukkashu@gmail.com>
2020-12-19 21:06:05 +0900

    update

--- main.py
+++ main.py
@@ -85,12 +85,12 @@ def callback():
     return 'OK'
 
 
-@handler.add(MessageEvent, message=TextMessage)
-def message_text(event):
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=event.message.text)
-    )
+# @handler.add(MessageEvent, message=TextMessage)
+# def message_text(event):
+#     line_bot_api.reply_message(
+#         event.reply_token,
+#         TextSendMessage(text=event.message.text)
+#     )
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_image(event):
