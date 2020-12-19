35a682e35f7621d84c00ea40202ac03c94a27521
yamabudousukkashu <yamabudousukkashu@gmail.com>
2020-12-19 23:57:18 +0900

    update

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage,
+    MessageEvent, TextMessage, TextSendMessage, ImageMessage,
 )
 from pathlib import Path
 import requests
