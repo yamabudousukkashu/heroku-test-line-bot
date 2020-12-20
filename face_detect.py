3016b8079bf75c9fec1c05eecf4427a1a18935dc
yamabudousukkashu <yamabudousukkashu@gmail.com>
2020-12-20 09:04:46 +0900

    ok

---
+++ face_detect.py
@@ -0,0 +1,5 @@
+import requestdef face_detect(image):
+
+    url = "https://api-us.faceplusplus.com/facepp/v3/detect"
+    payload = {'api_key': 'Foz12WqGEeBYxp65H_U9sWgPBP2Yr3_Q',\
+               'api_secret': 'wM6Ry-2rj8g-46tiTfBNphGhFUto5lG_'}
